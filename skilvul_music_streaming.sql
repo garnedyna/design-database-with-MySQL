50774bc8c6fcb7a66672e6a36468d27d4990448a
Garnedyna Aulia Rahma <garnedyna.ar@gmail.com>
2021-12-07 19:40:38 +0700

    update

--- skilvul_music_streaming.sql
+++ skilvul_music_streaming.sql
@@ -35,37 +35,39 @@ create table album (
 
 --Create table--
 create table playlist (
-  playlist_id int not null primary key auto_increment,
-    user_id int not null,
+    playlist_id int not null,
     name varchar(30),
-    constrain fk_playlist_user_user_id
+    user_id int not null,
+    constraint fk_playlist_user_user_id
     foreign key(user_id)
-    references user(user_id),   
-)
+    references user(user_id) 
+);
 --Create table--
 create table track (
-    track_id int not null primary key auto_increment,
+    track_id int not null,
     title varchar(30),
     singer_id int not null,
     album_id int not null,
-    constrain fk_track_singer_singer_id
+    constraint fk_track_singer_singer_id
     foreign key(singer_id)
     references singer(singer_id),
-    constrain fk_track_album_album_id
+    constraint fk_track_album_album_id
     foreign key(album_id)
-    references album(album_id),
+    references album(album_id)
 );
 
 --Create table conjunction--
-create table playlist_track(
-   playlist_id int not null,
-   track_id int not null,
-   constrain fk_playlist_id_Reff 
-   foreign key(playlist_id)
-   references playlist(playlist_id),
-   constrain fk_track_id_reff
-   foreign key(track_id)
-   references track(track_id)
+create table playlist_track
+(
+ id int not null auto_increment primary key ,
+ track_id    int not null ,
+ playlist_id int not null ,
+ constraint fk_track_conj
+ foreign key (track_id) 
+ references track (track_id),
+ constraint fk_playlist_conj
+ foreign key (playlist_id) 
+ references playlist (playlist_id)
 );
 
 --Show table--
